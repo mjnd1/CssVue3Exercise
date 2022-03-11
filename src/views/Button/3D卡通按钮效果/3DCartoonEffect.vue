26ef7a785ec5cf19c2ece0f8b259cec751478657
chenzhiwei <577514793@qq.com>
2022-03-11 17:08:21 +0800

    feat:新增3D卡通按钮片段

---
+++ src/views/Button/3D卡通按钮效果/3DCartoonEffect.vue
@@ -0,0 +1,24 @@
+<!--
+ * @Description: 
+ * @Version: 2.0
+ * @Autor: ChenZhiWei
+ * @Date: 2022-03-11 12:13:25
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-03-11 17:00:53
+-->
+<template>
+	<div>
+		<button class="3D-btn ">Don`t</button>
+		<button class="3D-btn ">Push</button>
+		<button class="3D-btn ">Me</button>
+	</div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+	name: "",
+	props: {},
+	setup(props, context) {},
+});
+</script>
+<style src="./3DCartoonEffect.module.css" scoped></style>
