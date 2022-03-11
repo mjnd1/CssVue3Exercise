c098b85401ab24f79b6e2d5dedbd0ae449e5f27d
chenzhiwei <577514793@qq.com>
2022-03-11 17:07:03 +0800

    mod修改启始页面

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
  * @version: 
  * @Author: ChenZhiWei
  * @Date: 2022-03-05 12:41:09
- * @LastEditors: ChenZhiWei
- * @LastEditTime: 2022-03-05 22:42:43
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-03-11 12:21:23
 -->
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
@@ -14,10 +14,10 @@ console.log(import.meta.env);
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+	<img alt="Vue logo" src="./assets/logo.png" />
+	<HelloWorld msg="Hello Vue 3 + Vite" />
   <!-- 路由入口文件 -->
-  <router-link to="/test1"> btn </router-link>
+  <!-- <router-link to="/button"> Button Css 效果 </router-link> -->
   <router-view />
 </template>
 
