40d8760532ff29582a62c26a15f8a4deb11b81ff
mjnd1 <577514793@qq.com>
2022-03-20 15:21:54 +0800

    mod:vite配置PostCSS的postcss-nested插件

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,7 @@
  * @Author: ChenZhiWei
  * @Date: 2022-03-20 12:56:25
  * @LastEditors: ChenZhiWei
- * @LastEditTime: 2022-03-20 14:26:56
+ * @LastEditTime: 2022-03-20 15:09:00
  */
 module.exports = (ctx) => {
 	console.log(">>> postcss.config.js的配置", ctx);
@@ -18,6 +18,11 @@ module.exports = (ctx) => {
 				// 最佳实践是使用.browserslistrcconfig 或browserslistkey inpackage.json与 Babel、ESLint 和 Stylelint 共享目标浏览器。
 				// 有关可用查询和默认值，请参阅 Browserslist 文档 。
 			},
+			// 配置 postcss-nested => 作用: 可以像sass一样写嵌套
+			"postcss-nested": {
+				// 默认情况下，插件会去除中间嵌套级别生成的任何空选择器。您可以设置preserveEmpty为true保留它们。
+				preserveEmpty: true,
+			}
 		}
 		// [
 		// 	ctx.env === 'production' ? require('postcss-plugin')() : false,
